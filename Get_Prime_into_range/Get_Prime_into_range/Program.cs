27ebd9bf8be6cf5ca208096c19bac4c8c67c1701
MohamedAboSeada <120672803+MohamedAboSeada@users.noreply.github.com>
2022-12-15 19:37:01 +0200

    Add files via upload

---
+++ Get_Prime_into_range/Get_Prime_into_range/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Program {
+    static void Main() {
+        int w;
+        Console.Write("Enter start point : ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter end point : ");
+        int n2 = int.Parse(Console.ReadLine());
+        for (int i = n1; i <= n2; i++) {
+            if (i == 1 || i == 0) {
+                continue;
+            }
+            w = 1;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    w = 0;
+                    break;
+                }
+            }
+            if (w == 1) {
+                Console.WriteLine("'{0}' => {1}",w,i);
+            }
+        }
+        Console.ReadLine();
+    }
+}
